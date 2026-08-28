8e7d8d655c5b0936f14eb5c43a2fc24ea6ee801b
cajub311 <69664397+cajub311@users.noreply.github.com>
2026-08-28 15:03:08 -0500

    Add Minnesota Recovery Connection as a standalone events source module.

---
+++ api/recovery-source.js
@@ -0,0 +1,9 @@
+module.exports = {
+  type: "tribe",
+  name: "Minnesota Recovery Connection",
+  cat: "Recovery",
+  base: "https://www.minnesotarecovery.org",
+  lat: 44.9637,
+  lng: -93.1768,
+  addr: "800 Transfer Rd, St Paul",
+};
